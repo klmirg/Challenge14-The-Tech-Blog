00c36d891794ef41b3115d3a5bf122666554b533
Ronya <klmirg6@gmail.com>
2022-01-13 01:49:26 -0600

    got the user routes working

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// turns on routes
+app.use(routes);
+
+// turns on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
