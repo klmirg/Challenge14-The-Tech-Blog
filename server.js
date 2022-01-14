c4b20bb64a6f9d6cc2512c232ddf4b86ea130e3c
Ronya <klmirg6@gmail.com>
2022-01-14 16:23:12 -0600

    Fixing up the code

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
